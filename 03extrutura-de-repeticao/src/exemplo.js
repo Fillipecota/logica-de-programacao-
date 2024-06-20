806cb65d3243ffbabedf5d6e6a93c6b2338de3fc
FILLIPE SANT'ANNA COTA <filipe_s_cota@estudante.sc.senai.br>
2024-06-20 19:47:07 -0300

    bora ali

--- 03extrutura-de-repeticao/src/exemplo.js
+++ 03extrutura-de-repeticao/src/exemplo.js
@@ -33,3 +33,21 @@ while (nota < 0.0 || nota > 10.0) {
     nota = leia.questionFloat("DIGITE A NOTA NOVAMENTE: ")
 }
 console.log("NOTA VALIDA")
+
+
+
+// diferença entre for e while
+
+var num = leia.questionInt("DIGITE UM NUMERO: ")
+var mult = 1
+
+while (mult <= 10) {
+    console.log(num + " x " + mult + " = " + mult * num)
+    mult++
+}
+
+var nume = leia.questionInt("DIGITE UM NUMERO: ")
+
+for(var i = 1; i <= 10; i++ ){
+console.log(nume + " x " + i + " = " + nume * i )
+}
