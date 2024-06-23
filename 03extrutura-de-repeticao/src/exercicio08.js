4b5a4643e34bb907e781d0cb3c47565d871ab3ca
Fillipe <filipe_s_cota@estudante.sc.senail.br>
2024-06-22 22:34:10 -0300

    exercicio continuação

--- 03extrutura-de-repeticao/src/exercicio08.js
+++ 03extrutura-de-repeticao/src/exercicio08.js
@@ -5,10 +5,3 @@ var quantidade  = 1
 var par
 var impar
 
-while ( quantidade <= 6){
-
-    if ( numero % 2 === 0){
-        numero = par
-
-    }
-}
---
+++ 03extrutura-de-repeticao/src/exercicio09.js
@@ -0,0 +1,56 @@
+import leia from "readline-sync"
+
+var num
+var vez 
+
+for (var i = vez; num >= 100 || num <= 200; i ++){
+    nu
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var numero
+// var vezess
+
+// do {
+
+//     if (numero >= 100 || numer <= 200){
+//         vezess ++
+//     }
+//     if ( numer === 0 && vezess != 0)
+//         console.log (vezess)
+
+// }while(numero != 0){
+//    nuemro = leia.questionInt
+// }
+
+
+
+// var numeContado =0
+
+// // do{
+// //     var numero = leia.questionInt("DIGITE UM NUMERO: ")
+// //     if (numero >= 100 || numero <= 200){
+// //         numeContado + 1
+
+// //         }
+    
+// // }while (numero === 0){
+// //     console.log("OS NUMEROS DE 100 A 200 FORAM: " + numeContado)
+// // }
+
