f4ab3e10d0b84462726c373f1d53d48678b06aa7
FILLIPE SANT'ANNA COTA <filipe_s_cota@estudante.sc.senai.br>
2024-06-26 21:56:31 -0300

    exercicioo

--- 03extrutura-de-repeticao/src/exercicio15.js
+++ 03extrutura-de-repeticao/src/exercicio15.js
@@ -1,12 +1,24 @@
 import leia from "readline-sync"
 
 var decimal = leia.questionInt("DIGITE O DECIMAL: ")
-var binario 
+var binario =""
 
-do {
-    binario = decimal % 2
 
-} while (binario === 0|| binario === 1);
-    console.log( binario)
 
-    
+    while (decimal>0){
+        var resultado= parseInt(decimal/2)
+        var resto = decimal %2
+        binario = resto+ binario
+        decimal = resultado
+    }
+console.log(binario)
+
+
+
+
+
+// do {
+//     binario = decimal % 2
+
+// } while (binario === 0|| binario === 1);
+//     console.log( binario)
