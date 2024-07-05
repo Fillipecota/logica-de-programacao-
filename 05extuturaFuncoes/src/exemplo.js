a2144e3f18bdfd1fb1b04da90173e99ea677c8c0
FILLIPE SANT'ANNA COTA <filipe_s_cota@estudante.sc.senai.br>
2024-07-04 21:48:13 -0300

    05/07

---
+++ 05extuturaFuncoes/src/exemplo.js
@@ -0,0 +1,36 @@
+import leia from "readline-sync"
+
+
+function mostra(){
+    var nome = leia.question("digite seu nome: ")
+    console.log("óla " + nome)
+}
+mostra();
+
+
+function Somar(num1, num2){
+ var result = num1 + num2;
+ console.log(" O RESULTADO É: " + result)
+}
+
+soma(10, 17);
+
+var nume1 = leia.questionInt("DIGITE UM NUMERO: ")
+var nume2 = leia.questionInt("DIGITE UM NUMERO: ")
+
+somar(nume1, nume2)
+
+function subtrair (num1,num2){
+    var result = num1 - num2;
+    console.log(" O RESULTADO É: " + result)
+}
+
+subtrair(10,5)
+
+function multi (num1, num2){
+    var result = num1* num2;
+    return result;
+}
+
+var resultado = multi(5,5)
+console.log(resultado)
---
+++ 05extuturaFuncoes/src/exercicio01.js
@@ -0,0 +1,25 @@
+import leia from "readline-sync"
+
+var numeroExercicio = leia.keyInSelect(1-exercicio01, 2-exercicio02)
+
+switch (numeroExercicio) {
+    case 1:
+        exercicio01()        
+        break;
+    case 2:
+        exercicio02()
+        break
+    default:
+        break;
+}
+
+
+    function exercicio01() {
+
+
+    }
+
+
+    function exercicio02(){
+        
+    }
