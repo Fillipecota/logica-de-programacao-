89a770479e2573154f849a7b80befa150d2b87c6
Fillipe <filipe_s_cota@estudante.sc.senail.br>
2024-07-06 01:10:36 -0300

    05/07/24

--- 05extuturaFuncoes/src/exercicio01.js
+++ 05extuturaFuncoes/src/exercicio01.js
@@ -16,10 +16,61 @@ switch (numeroExercicio) {
 
     function exercicio01() {
 
+        var numero = []
+        var numMulti= []
+      
+        adcNumero()
+         forMult()
+         console.log()
 
     }
 
 
     function exercicio02(){
+       
+        var numero = []
+        var numero2 = 0
+        adcNumero()
+        soma()
+        console.log(soma)
         
+        
+
+
+
+        
+        
+    }
+
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+    function adcNumero(){
+     var x =leia.questionInt("DIGITE UM VALOR A CIMA QUE A REPETIÇÃO DEVE PARAR: ")
+         for(var i = o; i< x; i++){
+         numero = leia.questionInt("DIGITE UM NUMERO: ")
+        }
+    }
+
+    function forMult(){
+     var x =leia.questionInt("DIGITE UM VALOR A CIMA QUE A REPETIÇÃO DEVE PARAR: ")
+     var z = leia.questionInt("DIGITE POR QUANTO QUER MULTIPLICAR: ")      
+         for(var i = 0; i<x; i++){
+         numMulti[i] = numero[i]*z  
+    }
+
+    }
+
+    function soma(){
+    var x =leia.questionInt("DIGITE UM VALOR A CIMA QUE A REPETIÇÃO DEVE PARAR: ")
+         for(var i = 0; i < x; i++){
+         numero2 = numero[i] + numero2
+         }
     }
